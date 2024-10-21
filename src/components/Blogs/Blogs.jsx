3b15db334ff86dde10a08f9181357d36dd66af9f
developernagor <developernagor@gmail.com>
2024-10-21 23:23:45 +0600

    Completed header section and started blog section

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,17 +3,23 @@ import { useState } from "react";
 
 
 const Blogs = () => {
-    const {blogs, setBlogs} = useState([]);
+    const [blogs, setBlogs] = useState([]);
 
     useEffect( () => {
         fetch('blogs.json')
         .then(res => res.json())
         .then(data => setBlogs(data))
     },[])
-    
+
     return (
-        <div>
-            
+        <div className="mx-10 py-10">
+            <h1>Blogs: {blogs.length}</h1>
+
+            <div className="flex">
+                <div className="w-2/3">Blog Item</div>
+                <div className="w-1/3">Bookmarks</div>
+            </div>
+
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
-
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
+        <header>
+            <div className='md:flex justify-between items-center mx-10 py-5 border-b-2'>
             <h1 className="text-3xl font-bold">Knowledge Cafe</h1>
-        </div>
+            <img src={profile} alt="" />
+            </div>
+        </header>
     );
 };
 
